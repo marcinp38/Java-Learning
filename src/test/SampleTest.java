221363d9b8ce57c916a5fad55ec4fdb510781fd5
marcinp38 <marcinp38@gmail.com>
2022-06-09 10:26:41 +0200

    TestNG -course

---
+++ src/test/SampleTest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class SampleTest {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+    }
+    @Test
+    public void sampleTest() {
+        System.out.println("Hello!");
+    }
+}
