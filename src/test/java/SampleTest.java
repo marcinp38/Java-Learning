ad3683ecf784980dfc404704d639e4d181b2698c
marcinp38 <marcinp38@gmail.com>
2022-06-09 10:55:23 +0200

    TestNG -course

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,4 +1,6 @@
-package test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class SampleTest {
     public static void main(String[] args) {
